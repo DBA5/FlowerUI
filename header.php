3074ae90994b6d731afcc3db9f01931e304d68b8
Stephanie Giraldo <stephaniegiraldo@ufl.edu>
2017-12-12 22:46:41 -0500

    added bootstrap

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="">
 		<meta name="author" content="">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 
 		<title>CIS4301 A5</title>
 		// ?????? 
